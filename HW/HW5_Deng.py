e3c8b413a02d71097c7acd15df098e162b7c880b
weiyedeng <dweiye.wustl@gmail.com>
2021-08-30 17:29:23 -0500

    Update HW5_Deng.py

--- HW/HW5_Deng.py
+++ HW/HW5_Deng.py
@@ -19,9 +19,11 @@ class LinkedList:
     def __init__(self, initial_value):
         self.value = initial_value
         self.linkedlist = [initial_value.value]
+        ## Complexity: O(1), because it always takes a vlue and inputs and done
     
     def length(self):
         return len(self.linkedlist)
+        ## Complexity：O(1), because len() only assesses once
     
     def addNode(self,new_value):
         if isinstance(new_value.value, int) == True:
@@ -29,38 +31,58 @@ class LinkedList:
             return self.linkedlist
         else:
             return "You node should contain an integer."
+        ## Complexity: O(1), because it only evaluates append() method once
     
     def addNodeAfter(self, new_value, after_node):
         position_to_add = self.linkedlist.index(after_node.value)
         self.linkedlist.insert(position_to_add+1, new_value.value)
         return self.linkedlist
+        ## Complexity: O(1), because regardless of the length of the input,
+        ## the insert() method is only evaluated once
     
     def addNodeBefore(self, new_value, after_node):
         position_to_add = self.linkedlist.index(after_node.value)
         self.linkedlist.insert(position_to_add, new_value.value)
         return self.linkedlist
+        ## Complexity: O(1), with similar reasons above
     
     def removeNode(self, node_to_remove):
         try:
-            return self.linkedlist.remove(node_to_remove.value)
+            self.linkedlist.remove(node_to_remove.value)
+            return self.linkedlist
         except ValueError:
             return "You do not have this node in your list."
-    
+        ## Complexity: O(1), because the remove() method is only evaluted once
+        
+    def removeNodesbyValue(self, value):
+        for elem in self.linkedlist:
+            if elem == value:
+                self.linkedlist.remove(value)
+        return self.linkedlist
+        ## Complexity: O(n), because the method loops over the linkedlist to
+        ## decide which element to remove
+        ## I think this is the least complex way? Without looping over the list,
+        ## I will not know whether there is any element that can be matched.
+            
     def reverse(self):
         return self.linkedlist.reverse()
+        ## Complexity: O(1), Straightforward
     
     def __str__(self):
         return "The linkedlist is %s" % " -> ".join([str(elem) for elem in self.linkedlist])
-
+        ## Complexity: O(1), Straightforward
     
 
 
 #%%
 node_1 = Node(_value=1)
-node_2 = Node(_value=5)
-node_3 = Node(_value=9)
-node_4 = Node(_value=6)
-node_5 = Node(_value=7)
+node_2 = Node(_value=2)
+node_3 = Node(_value=3)
+node_4 = Node(_value=4)
+node_5 = Node(_value=5)
+node_6 = Node(_value=6)
+node_6_1 = Node(_value=6)
+node_
 
 # Test
 first_list = LinkedList(node_1)
@@ -68,8 +90,10 @@ first_list.addNode(node_2)
 first_list.addNode(node_3)
 first_list.addNodeAfter(node_4,node_1)
 first_list.addNodeBefore(node_5,node_2)
+first_list.addNodeBefore(node_6,node_4)
 
 first_list.length()
 first_list.reverse()
 first_list.removeNode(node_2)
+first_list.removeNodesbyValue(7)
 print(first_list)
